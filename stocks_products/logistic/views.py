90aa97fb1bd833881845e7c5a21d066efad81ef6
Pronin Slava <prosnin-v@bk.ru>
2022-09-19 19:02:09 +0300

    new

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -1,9 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
+
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer, ProductPositionSerializer
+
+
+
 
 
 
@@ -19,3 +24,8 @@ class StockViewSet(ModelViewSet):
     serializer_class = ProductPositionSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['products']
+
+
+
+
+
